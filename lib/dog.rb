eaeeed7ace9b9fba9dc4d3fd2381fedd46b045db
Don Ward <10is0420@gmail.com>
2018-12-31 12:37:33 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Dog
+  
+  @@all = []
+  
+  attr_accessor :name
+
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.all
+    @@all.each do |dog|
+      puts dog.name
+    end
+    #binding.pry
+  end
+  
+end
